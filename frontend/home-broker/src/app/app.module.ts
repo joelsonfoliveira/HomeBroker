33aeec389de1a9993871b1ac755bc3854cb843f3
JOELSON FREITAS DE OLIVEIRA <108674@pmenos.com.br>
2023-08-04 23:53:08 -0300

    [TORO-002] Atualizando frontend

--- frontend/home-broker/src/app/app.module.ts
+++ frontend/home-broker/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CardPatrimonioComponent } from './components/card-patrimonio/card-patrimonio.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -19,12 +18,7 @@ import { AboutComponent } from './components/pages/about/about.component';
     HomeComponent,
     AboutComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CardPatrimonioComponent,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
